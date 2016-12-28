9208f553774e73c65a925b6bf0a2e3bd34ab788e
gitaeuber <gitaeuber@users.noreply.github.com>
2016-12-28 01:14:35 +0100

    fix bug parsing the data type option; reverse the order to distinguish between the different int types

--- mbrtu.c
+++ mbrtu.c
@@ -653,25 +653,7 @@ inline int parse_call_parameter_options (int argc, char *argv[], int option)
 	    case 't':
 		call->type = MBRTU_TYPE_HEX;
 
-		if      (! strncasecmp ("int",      optarg, 3))
-		    call->type = MBRTU_TYPE_INT16;
-		else if (! strncasecmp ("uint",     optarg, 4))
-		    call->type = MBRTU_TYPE_UINT16;
-		else if (! strncasecmp ("CHAR",     optarg, 4))
-		    call->type = MBRTU_TYPE_CHAR;
-		else if (! strncasecmp ("int16",    optarg, 5))
-		    call->type = MBRTU_TYPE_INT16;
-		else if (! strncasecmp ("int32",    optarg, 5))
-		    call->type = MBRTU_TYPE_INT32;
-		else if (! strncasecmp ("int64",    optarg, 5))
-		    call->type = MBRTU_TYPE_INT64;
-		else if (! strncasecmp ("uint16",   optarg, 6))
-		    call->type = MBRTU_TYPE_UINT16;
-		else if (! strncasecmp ("uint32",   optarg, 6))
-		    call->type = MBRTU_TYPE_UINT32;
-		else if (! strncasecmp ("uint64",   optarg, 6))
-		    call->type = MBRTU_TYPE_UINT64;
-		else if (! strncasecmp ("F32_ABCD", optarg, 8))
+		if	(! strncasecmp ("F32_ABCD", optarg, 8))
 		    call->type = MBRTU_TYPE_F32_ABCD;
 		else if (! strncasecmp ("F32_BADC", optarg, 8))
 		    call->type = MBRTU_TYPE_F32_BADC;
@@ -679,6 +661,24 @@ inline int parse_call_parameter_options (int argc, char *argv[], int option)
 		    call->type = MBRTU_TYPE_F32_CDAB;
 		else if (! strncasecmp ("F32_DCBA", optarg, 8))
 		    call->type = MBRTU_TYPE_F32_DCBA;
+		else if (! strncasecmp ("uint16",   optarg, 6))
+		    call->type = MBRTU_TYPE_UINT16;
+		else if (! strncasecmp ("uint32",   optarg, 6))
+		    call->type = MBRTU_TYPE_UINT32;
+		else if (! strncasecmp ("uint64",   optarg, 6))
+		    call->type = MBRTU_TYPE_UINT64;
+		else if (! strncasecmp ("int16",    optarg, 5))
+		    call->type = MBRTU_TYPE_INT16;
+		else if (! strncasecmp ("int32",    optarg, 5))
+		    call->type = MBRTU_TYPE_INT32;
+		else if (! strncasecmp ("int64",    optarg, 5))
+		    call->type = MBRTU_TYPE_INT64;
+		else if (! strncasecmp ("uint",     optarg, 4))
+		    call->type = MBRTU_TYPE_UINT16;
+		else if (! strncasecmp ("CHAR",     optarg, 4))
+		    call->type = MBRTU_TYPE_CHAR;
+		else if (! strncasecmp ("int",      optarg, 3))
+		    call->type = MBRTU_TYPE_INT16;
 
 //		set_flags |= MBRTU_SET_TYPE;
 		break;
